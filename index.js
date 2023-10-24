3d83005ec927bef1d714c37b89f3ad4822e50b23
floflo0x <148141879+floflo0x@users.noreply.github.com>
2023-10-24 10:14:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+app.use("/login", (req, res, next) => {
+    console.log(req.body);
+    res.send("login");
+})
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
